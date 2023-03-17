7a9ab564d3b1bf2bcc0c0e32d7d3537b854eeefc
Mateus Rebouças <110422001+mateusreboucas08@users.noreply.github.com>
2023-03-17 18:20:50 -0300

    Add files via upload

---
+++ aula4soma.rb
@@ -0,0 +1,6 @@
+print "Digite um número inteiro: "
+num1 = gets.chomp.to_i
+print "Digite mais um número: "
+num2 = gets.chomp.to_i
+soma = num1 + num2
+puts "O resultado será #{soma}"
---
+++ desafio1.rb
@@ -0,0 +1,8 @@
+print "Digite o seu nome: "
+nome = gets.chomp
+print "Digite o seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite a sua idade: "
+idade = gets.chomp.to_i
+
+puts "Olá #{nome} #{sobrenome}, vimos que tem #{idade} anos, gostaria de ver o nosso setor dessa idade?"
